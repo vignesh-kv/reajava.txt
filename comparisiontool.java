fed037748892ac7b967b43eadcaf440ddf3f68e5
vignesh-kv <kv.vignesh2004@gmail.com>
2025-01-30 20:39:14 +0530

    Add files via upload

---
+++ comparisiontool.java
@@ -0,0 +1,25 @@
+class ComparisonTool {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 1;
+
+        
+        if (a == b) {
+            System.out.println("a == b is true");
+        }
+        if (a >= b) {
+            System.out.println("a >= b is true");
+        }
+        if (a < b) {
+            System.out.println("a < b is true");
+        }
+        if (a <= b) {
+            System.out.println("a <= b is true");
+        }
+        if (a != b) {
+            System.out.println("a != b is true");
+        } else {
+            System.out.println("invalid");
+        }
+    }
+}
